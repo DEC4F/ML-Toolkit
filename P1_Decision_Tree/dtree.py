8d2ff92bb0ebfb97d024f4b7156a6bd27967bdff
DEC4F <rarunok@yahoo.com>
2018-09-11 14:43:40 -0400

    added gitignore

--- P1_Decision_Tree/dtree.py
+++ P1_Decision_Tree/dtree.py
@@ -4,18 +4,19 @@ This is an implementation of ID3 decision tree https://en.wikipedia.org/wiki/ID3
 Author: Stan Tian, Yimin Chen, Devansh Gupta
 """
 
-import sys
+import sys, os
+import mldata
 from DT_Model import ID3
-from Data_Loader import Loader
 
 def main():
     """
     run the decision tree with param given by user
     """
+    # file_path = '/home/decaf/Dropbox/EECS 440/dataset/voting'
     file_path, use_full_sample, max_depth, use_gain_ratio = sys.argv[1:]
-    loader = Loader(file_path)
-    X = loader.samples
-    y = loader.labels
+    samples = mldata.parse_c45(file_path.split(os.sep)[-1], file_path)
+    X = samples[:, 1:-1]
+    y = samples[:, -1]
     print(y)
     #dt = ID3(max_depth, use_gain_ratio)
     #dt.fit(X, y)
