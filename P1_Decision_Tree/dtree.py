e49cecb7c58c353641a9e48fbfd8d7dccb0930b5
DEC4F <rarunok@yahoo.com>
2018-09-11 15:09:26 -0400

    turned samples into np array

--- P1_Decision_Tree/dtree.py
+++ P1_Decision_Tree/dtree.py
@@ -4,7 +4,9 @@ This is an implementation of ID3 decision tree https://en.wikipedia.org/wiki/ID3
 Author: Stan Tian, Yimin Chen, Devansh Gupta
 """
 
-import sys, os
+import sys
+import os
+import numpy as np
 import mldata
 from DT_Model import ID3
 
@@ -12,12 +14,10 @@ def main():
     """
     run the decision tree with param given by user
     """
-    # file_path = '/home/decaf/Dropbox/EECS 440/dataset/voting'
-    file_path, use_full_sample, max_depth, use_gain_ratio = sys.argv[1:]
-    samples = mldata.parse_c45(file_path.split(os.sep)[-1], file_path)
+    file_path, use_full_sample, max_depth, use_gain_ratio = sys.argv[1:5]
+    samples = np.array(mldata.parse_c45(file_path.split(os.sep)[-1], file_path))
     X = samples[:, 1:-1]
     y = samples[:, -1]
-    print(y)
     #dt = ID3(max_depth, use_gain_ratio)
     #dt.fit(X, y)
 
