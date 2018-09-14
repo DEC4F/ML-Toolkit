ac5864dd69eab212682edc7aa4763845fcbe33df
devanshg <devanshg@Murtuzas-MacBook-Pro.local>
2018-09-14 14:26:20 -0400

    added functions to calculate IG for Continuous attributes

--- P1_Decision_Tree/DT_Model.py
+++ P1_Decision_Tree/DT_Model.py
@@ -63,7 +63,8 @@ class ID3(object):
         else:
             #TODO: partition cont value
             part_ent = None
-            return None
+            return self.ig_of_cont_attr(attr, labels)
+            #return None
 
     def ig_of_discrete_attr(self, attr, labels):
         """
@@ -85,6 +86,46 @@ class ID3(object):
                 best_ig = curr_ig
         return best_ig
 
+    def ig_of_cont_attr(self, attr, labels):
+        """
+        calculates entropy of input continuous labels
+        ----------
+        labels : array-like
+            a list of labels
+        """
+        og_ent = self.entropy_of(labels)
+        cont_attr_label_pair = np.array(attr, labels).T
+        # -2 : I took an example where second last column is for the continuous attribute                                                           
+        sortted_attr_label_pair = cont_attr_label_pair[cont_attr_label_pair[:,0].argsort(kind='mergesort')]
+        # Sort the continuous attribute label based in the ascending order
+        chng_in_val = np.where(np.roll(sortted_attr_label_pair[:,1],1)!=sortted_attr_label_pair[:,1])[0]
+        # chng_in_val :  list of the indexes of the continious attributes where there's a change in the Class Label
+        best_ig = 0.0
+        # traverse through all the indexes of the continious attributes where there's a change in the Class Label
+        for i in chng_in_val[1:]:
+            # attribute list with attribute less than or equal-to the found index-of-change
+            attr_list_with_TRUE = sortted_attr_label_pair[sortted_attr_label_pair[:,0]<= ((sortted_attr_label_pair[:,0][i] + sortted_attr_label_pair[:,0][i-1])/2)]
+            
+            # attribute list with attribute greater than the found index-of-change
+            attr_list_with_False = sortted_attr_label_pair[sortted_attr_label_pair[:,0] > ((sortted_attr_label_pair[:,0][i] + sortted_attr_label_pair[:,0][i-1])/2)]
+           
+            # probability of attribute list with attribute less than or equal-to the found index-of-change
+            probab_with_TRUE = (attr_list_with_TRUE.shape[0])/float(sortted_attr_label_pair.shape[0])
+
+            # probability of attribute list with attribute greater than the found index-of-change
+            probab_with_False = (attr_list_with_False.shape[0])/float(sortted_attr_label_pair.shape[0])
+            
+            # As H(Y|X) = P(X=TRUE)H(Y|X=TRUE) + P(X=FALSE)H(Y|X=FALSE)
+            curr_et = probab_with_TRUE*entropy_of_cont(attr_list_with_TRUE) + probab_with_False*entropy_of_cont(attr_list_with_False)
+            
+            #IG = H(Y) - H(Y|X)
+            ig_cont = og_ent - curr_et
+            
+            # Finding the maximum information gain
+            if best_ig < ig_cont:
+                best_ig = ig_cont
+            return best_ig
+        
     def entropy_of(self, labels):
         """
         calculates entropy of input labels
@@ -97,3 +138,20 @@ class ID3(object):
         prob = list(map(lambda x: x/float(np.sum(occurence)), occurence))
         entropy = -np.sum(list(map(lambda x: x*np.log2(x), prob)))
         return entropy
+    def entropy_of_cont(self, labels):
+    """
+    calculates entropy of input labels
+    ----------
+    labels : array-like
+        a list of labels
+    """
+    from collections import Counter
+    occurence = list(Counter(labels[:,0]).values())
+    prob = [x/float(np.sum(occurence)) for x in occurence]
+    boolarr = np.array(labels[:,1], dtype=np.bool)
+    if np.sum(boolarr) ==0:
+        entropy = 0.0
+    else:
+        entropy = -np.sum([x*np.log2(np.sum(boolarr)/float(np.sum(occurence))) for x in prob])
+    return entropy
+
